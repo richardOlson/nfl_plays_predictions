103959f158e7ad7aa19b3c61774e53b3e5e38cdb
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-03-03 22:45:05 -0700

    changed more in predict file

--- pages/predictions.py
+++ pages/predictions.py
@@ -22,6 +22,8 @@ column1 = dbc.Col(
             Use the drop-downs and sliders to choose the information you want to add. If you don't change
             anything the defaults shown will be used. 
 
+
+
             """
         ),
                 
@@ -33,26 +35,204 @@ column1 = dbc.Col(
 
 column2 = dbc.Col(
     [
-        dcc.Dropdown(
-            options=[
-            {'label': 'First Quarter', 'value': 1},
-            {'label': 'Second Quarter', 'value': 2},
-            {'label': 'Third Quarter', 'value': 3},
-            {'label': 'Fourth Quarter', 'value':4},
-            {'label': 'Overtime', 'value':5}
-            ],
-            value=1,
-            placeholder="Select a Quarter",
-            style={"background-color": "#f2f2f2", 'color':'black'}
+        
+        dcc.Markdown(
+            """
+        
+            Information about current play
 
+            
+
+            """
         ),
-        daq.NumericInput(
-            id='minute',
-            max=15,
-            min=0,
-            value=8
-        )  
+
+        html.Div(children=[
+        html.Div(
+            id="time",
+            children=[
+                dcc.Dropdown(
+                    id='quarter',
+                    options=[
+                        {'label': '1st Qtr', 'value': 1},
+                        {'label': '2nd Qtr' , 'value': 2},
+                        {'label': '3rd Qtr', 'value': 3},
+                        {'label': '4th Qtr', 'value': 4},
+                        {'label': 'Overtime', 'value': 5},
+                    ],
+                    searchable=True,
+                    placeholder="Quarter",
+                    style={'width': '63%',  'color':'black', 'marginBottom': '1em'}
+                ), 
+
+            
+            # The minue input
+                dcc.Input(
+                    id='minute',
+                    placeholder='Min',
+                    persistence=True,
+                    max=15,
+                    min=0,
+                    step=1,
+                    type='number', 
+                    style={'width': '20%', 'display': 'inline-block','marginBottom': '1em'}
+                ),#style={'width': '49%', 'display': 'inline-block'}
+
+                # The seconds input
+                  dcc.Input(
+                    id='seconds',
+                    placeholder='Sec',
+                    persistence=True,
+                    type='number',
+                    max=59, 
+                    min=0,
+                    step=1,
+                    value='', 
+                    style={'width': '20%', 'display': 'inline-block','marginBottom': '1em'}
+                    ), 
+                
+                
+                ]
+        ), 
+            html.Div(
+                # The down
+                dcc.Dropdown(
+                    id='down',
+                    options=[
+                        {'label': '1st Down', 'value': 1},
+                        {'label': '2nd Down', 'value': 2},
+                        {'label': '3rd Down', 'value': 3},
+                        {'label': '4th Down', 'value': 4},
+                        
+                    ],
+                    searchable=True,
+                    placeholder="Down",
+                    style={'width': '63%',  'color':'black' ,'marginBottom': '1em'}
+                ),
+
+                    
+            ),
+
+            dcc.Input(
+                        id='yard_to_go',
+                        placeholder='Yards to Go',
+                        persistence=True,
+                        type='number',
+                        max=100, 
+                        min=0,
+                        step=1,
+                        value='', 
+                        style={'width': '40%', 'display': 'inline-block','marginBottom': '1em'}
+                        ), 
+
+
+            dcc.Dropdown(
+                    id='formation',
+                    options=[
+                        {'label': 'Under Center', 'value': 'under_center'},
+                        {'label': 'Shotgun', 'value': 'shotgun'},
+                        {'label': 'No Huddle Shotgun', 'value': 'no_huddle_shotgun'},
+                        {'label': 'No Huddle', 'value': 'no_huddle'},
+                        {'label': 'Wildcat', 'value': 'wildcat'},
+                        {'label': 'Other', 'value': 'nan'},
+                    ],
+                    searchable=True,
+                    placeholder="Formation",
+                    style={'width': '65%',  'color':'black', 'display': 'inline-block'}
+                ), 
+
+
+
+        #style={'display': 'inline-block'})    
+        ])
+
+        
+        
     ]
 )
 
-layout = dbc.Row([column1, column2])
+column3 = dbc.Col(
+        [
+            dcc.Markdown(
+            """
+        
+            Information from previous  offensive play
+
+            
+
+            """
+        ),
+
+        dcc.Dropdown(
+                    id='prev_down',
+                    options=[
+                        {'label': '1st Down', 'value': 1},
+                        {'label': '2nd Down', 'value': 2},
+                        {'label': '3rd Down', 'value': 3},
+                        {'label': '4th Down', 'value': 4},
+                        {'label': 'Two Point conv', 'value': 5},
+                        
+                    ],
+                    searchable=True,
+                    placeholder="Down on Prev Play",
+                    style={'width': '78%',  'color':'black' ,'marginBottom': '1em'}
+                ),
+
+
+        dcc.Input(
+                        id='yards_needed',
+                        placeholder='Yards for 1st down',
+                        persistence=True,
+                        type='number',
+                        max=100, 
+                        min=0,
+                        step=1,
+                        value='', 
+                        style={'width': '60%', 'display': 'inline-block','marginBottom': '1em'}
+                        ),
+
+        dcc.Input(
+                        id='yards_gained',
+                        placeholder='Yards gained',
+                        persistence=True,
+                        type='number',
+                        max=100, 
+                        min=-99,
+                        step=1,
+                        value='', 
+                        style={'width': '60%', 'display': 'inline-block','marginBottom': '1em'}
+                        ),
+
+
+        html.Div(
+            dcc.Dropdown(
+                        id='prev_play',
+                        options=[
+                            {'label': 'Pass', 'value': 'Pass'},
+                            {'label': 'Run', 'value': 'Rush'},
+                            
+                        ],
+                        searchable=True,
+                        placeholder="Previous Play Type",
+                        style={'width': '78%',  'color':'black', 'display': 'inline-block','marginBottom': '.3em'}
+                    ), 
+        ),
+        dcc.Dropdown(
+                id='what_happened',
+                options=[
+                    {'label':'Normal Play', 'value': 'nothing'},
+                    {'label':'Touch Down', 'value': 'touchDown'},
+                    {'label':'Fumble', 'value': 'fumble'},
+                    {'label':'Interception', 'value': 'interception'},
+                    {'label':'Safety', 'value': 'nothing'},
+
+                ], 
+                searchable=True,
+                placeholder="Result of Play",
+                style={'width': '78%',  'color':'black', 'display': 'inline-block','marginBottom': '.3em'}
+
+        ),
+
+        ]
+)
+
+layout = dbc.Row([column1, column2, column3])
