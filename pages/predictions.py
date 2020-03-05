9a5dd87a247258f1954bc6310f74cf226f5e75c2
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-03-04 17:30:23 -0800

    more changes

--- pages/predictions.py
+++ pages/predictions.py
@@ -3,12 +3,17 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_daq as daq
 
 
 # Imports from this application
 from app import app
+from pickle import load
+
+#load(open(fileName, 'rb')) C:\Users\rich\Richard_python\nfl_dash\nfl_plays_predictions\assets
+theXgb = load(open(r'assets\xgb.pkl', 'rb'))
+
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
@@ -19,14 +24,34 @@ column1 = dbc.Col(
         
             ## Predictions
 
-            Use the drop-downs and sliders to choose the information you want to add. If you don't change
-            anything the defaults shown will be used. 
+            Use the drop-downs and text boxes to enter the information you want to add. These will then 
+            be used to make the prediction if the next play is a Pass or a Run play.
+
+
 
 
 
             """
         ),
-                
+         html.Button(
+            'Predict',
+            id='predict_button',
+            style={'width': '63%',  'color':'black', 'background-color': '#de7518',
+                    'marginTop': '1em', 'marginBottom': '1em'}
+         ),
+
+
+
+        dcc.RadioItems(
+            options=[
+                {'label': 'Don\'t show probability',  'value': 'No'},
+                {'label': 'Show Probability', 'value': 'Yes'},
+
+            ],
+            value='No'
+        ),
+
+
     
     
     ],
@@ -61,11 +86,12 @@ column2 = dbc.Col(
                     ],
                     searchable=True,
                     placeholder="Quarter",
-                    style={'width': '63%',  'color':'black', 'marginBottom': '1em'}
+                    style={'width': '63%',  'color':'black', 'marginBottom': '1em',
+                           'font-size':'small'}
                 ), 
 
             
-            # The minue input
+            # The minute input
                 dcc.Input(
                     id='minute',
                     placeholder='Min',
@@ -74,7 +100,8 @@ column2 = dbc.Col(
                     min=0,
                     step=1,
                     type='number', 
-                    style={'width': '20%', 'display': 'inline-block','marginBottom': '1em'}
+                    style={'width': '20%', 'display': 'inline-block','marginBottom': '1em',
+                           'font-size':'small'}
                 ),#style={'width': '49%', 'display': 'inline-block'}
 
                 # The seconds input
@@ -87,7 +114,7 @@ column2 = dbc.Col(
                     min=0,
                     step=1,
                     value='', 
-                    style={'width': '20%', 'display': 'inline-block','marginBottom': '1em'}
+                    style={'width': '20%', 'display': 'inline-block','marginBottom': '1em', 'font-size':'small'}
                     ), 
                 
                 
@@ -121,7 +148,8 @@ column2 = dbc.Col(
                         min=0,
                         step=1,
                         value='', 
-                        style={'width': '40%', 'display': 'inline-block','marginBottom': '1em'}
+                        style={'width': '40%', 'display': 'inline-block','marginBottom': '1em',
+                               'font-size':'small'}
                         ), 
 
 
@@ -137,7 +165,8 @@ column2 = dbc.Col(
                     ],
                     searchable=True,
                     placeholder="Formation",
-                    style={'width': '65%',  'color':'black', 'display': 'inline-block'}
+                    style={'width': '65%',  'color':'black', 'display': 'inline-block',
+                           'font-size':'small'}
                 ), 
 
 
@@ -173,8 +202,9 @@ column3 = dbc.Col(
                         
                     ],
                     searchable=True,
-                    placeholder="Down on Prev Play",
-                    style={'width': '78%',  'color':'black' ,'marginBottom': '1em'}
+                    placeholder="Preious Down",
+                    style={'width': '78%',  'color':'black' ,'marginBottom': '1em',
+                           'font-size':'small'}
                 ),
 
 
@@ -187,7 +217,8 @@ column3 = dbc.Col(
                         min=0,
                         step=1,
                         value='', 
-                        style={'width': '60%', 'display': 'inline-block','marginBottom': '1em'}
+                        style={'width': '60%', 'display': 'inline-block','marginBottom': '1em' ,
+                               'font-size':'small'}
                         ),
 
         dcc.Input(
@@ -212,8 +243,9 @@ column3 = dbc.Col(
                             
                         ],
                         searchable=True,
-                        placeholder="Previous Play Type",
-                        style={'width': '78%',  'color':'black', 'display': 'inline-block','marginBottom': '.3em'}
+                        placeholder="Play Type",
+                        style={'width': '78%',  'color':'black', 'display': 'inline-block','marginBottom': '.3em',
+                               'font-size':'small'}
                     ), 
         ),
         dcc.Dropdown(
@@ -232,7 +264,34 @@ column3 = dbc.Col(
 
         ),
 
+
+
+
         ]
 )
 
-layout = dbc.Row([column1, column2, column3])
+layout = html.Div(
+    [
+
+
+    dbc.Row([column1, column2, column3]),
+    dbc.Row(dbc.Col(html.Div(id="predHere", ),
+                    width={"size": 6, "offset": 3},))
+
+    ]
+)
+
+
+
+
+@app.callback(Output('output-state', 'children'),
+              [Input('submit-button', 'n_clicks')],
+              [State('input-1-state', 'value'),
+               State('input-2-state', 'value')])
+def update_output(n_clicks, input1, input2):
+    return u'''
+        The Button has been pressed {} times,
+        Input 1 is "{}",
+        and Input 2 is "{}"
+    '''.format(n_clicks, input1, input2)
+
